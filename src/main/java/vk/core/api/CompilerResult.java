c684d006147a53d116e666225225060984b3e044
Jens Bendisposto <jens@bendisposto.de>
2016-07-03 10:43:56 +0200

    Added some explanation of the method, because the chosen name is wrong

--- src/main/java/vk/core/api/CompilerResult.java
+++ src/main/java/vk/core/api/CompilerResult.java
@@ -6,19 +6,23 @@ import java.util.Collection;
 public interface CompilerResult {
 
 	/**
+	 * The method's name is misleading. It should have been named
+	 * hasNoCompileErrors. We will keep the name though because changing it will
+	 * break the clients.
+	 * 
 	 * @return true, if and only if the compilation process produced no error
 	 *         messages
 	 */
 	boolean hasCompileErrors();
 
 	/**
-	 * @return Duration of the compilation process 
+	 * @return Duration of the compilation process
 	 */
 	Duration getCompileDuration();
 
-	
 	/**
-	 * @param cu - a compilation unit
+	 * @param cu
+	 *            - a compilation unit
 	 * @return all errors for that compilation unit
 	 */
 	Collection<CompileError> getCompilerErrorsForCompilationUnit(CompilationUnit cu);
